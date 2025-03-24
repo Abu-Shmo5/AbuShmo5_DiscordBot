78d8a0cf3ea991e99d9affb97cfe8991d98ee7ec
Abu Shmo5 <abushmo544@gmail.com>
2025-03-24 17:29:52 +0300

    time + colouring functions

--- lib/helper.py
+++ lib/helper.py
@@ -1,8 +1,13 @@
 import os
 import json
+import time
 import discord
+from lib import cli_colouring
 
 class Helper():
+    def get_time() -> str:
+        return time.strftime(f"[%Y-%m-%d %H:%M:%S]", time.localtime(time.time()))
+
     def is_guild(guild: discord.Guild | int | None) -> bool:
         return guild is not None
     
@@ -16,3 +21,6 @@ class Helper():
     def save_config(config):
         with open("config.json", "w") as file:
             file.write(json.dumps(config))
+
+    def print(content):
+        print(f"{cli_colouring.EFFECTS.FOREGROUND_BRIGHT_BLACK.value}{Helper.get_time()}{cli_colouring.EFFECTS.CLEAR.value} {content}")
